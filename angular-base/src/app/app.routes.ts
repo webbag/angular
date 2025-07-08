5764be3b6af1f5c2e89bd51c8a80aaba9f4aca66
Krzysztof Kromolicki <k.kromolicki@gmail.com>
2025-07-08 22:47:30 +0200

    routing

--- angular-base/src/app/app.routes.ts
+++ angular-base/src/app/app.routes.ts
@@ -2,12 +2,22 @@ import { Routes } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { DomainComponent } from './domain/domain.component';
 import { ApplicationComponent } from './application/application.component';
+import { UserFormComponent } from './user-form/user-form.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
   {
     path: 'user',
     component: UserComponent,
   },
+  {
+    path: 'user-form',
+    component: UserFormComponent,
+  },  
   {
     path: 'domain',
     component: DomainComponent,
---
+++ angular-base/src/app/dashboard/dashboard.component.spec.ts
@@ -0,0 +1,18 @@
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DashboardComponent } from './dashboard.component';
+
+describe('DashboardComponent', () => {
+  let component: DashboardComponent;
+  let fixture: ComponentFixture<DashboardComponent>;
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DashboardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+});
