ba9a5c5a95c9d48209290ad35ebe003d22b591c7
Krzysztof Kromolicki <k.kromolicki@gmail.com>
2025-07-12 20:33:50 +0200

    user, domain, application

--- angular-base/src/app/app.routes.ts
+++ angular-base/src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { DomainComponent } from './domain/domain.component';
 import { ApplicationComponent } from './application/application.component';
-import { UserFormComponent } from './user-form/user-form.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
@@ -14,10 +13,6 @@ export const routes: Routes = [
     path: 'user',
     component: UserComponent,
   },
-  {
-    path: 'user-form',
-    component: UserFormComponent,
-  },  
   {
     path: 'domain',
     component: DomainComponent,
